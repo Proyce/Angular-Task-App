224a8140532a1e3b30811914850968781a8c9b4b
proyce <prospernnadi3@gmail.com>
2022-11-23 03:28:43 +0100

    Added form

--- src/app/todos/components/todo/todo.component.ts
+++ src/app/todos/components/todo/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { isContext } from 'vm';
 
 @Component({
   selector: 'app-todo',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
-
+  isChecked: boolean = false;
   constructor() { }
 
+
+
   ngOnInit() {
+  }
+
+  toggleChecked(){
+    this.isChecked = !this.isChecked
+    console.log(this.isChecked);
   }
 
 }
